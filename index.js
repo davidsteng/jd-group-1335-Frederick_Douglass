290213595272e6886c2aaec035c6f897ac7979a9
sarjusuhag <89706507+sarjusuhag@users.noreply.github.com>
2022-01-27 18:29:59 -0500

    The Letter and Preface Implemented

---
+++ index.js
@@ -0,0 +1,10 @@
+import EpubView from './EpubView/EpubView'
+import EpubViewStyle from './EpubView/style'
+import EBookReader from './epubreader/epubreader'
+import EBookReaderStyle from './epubreader/style'
+import Home from './Home/Home'
+import Preface from './Preface/Preface'
+import Letter from './Letter/Letter'
+
+
+export { EpubView, EpubViewStyle, EBookReader, EBookReaderStyle, Home, Preface, Letter }
